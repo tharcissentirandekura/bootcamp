5eb7af5649a2bca33691aeb2d5ecad535ade3fa1
Ntirandekura Tharcisse <119756165+tharcissentirandekura@users.noreply.github.com>
2023-12-31 02:52:51 +0000

    adding

--- practice.py
+++ practice.py
@@ -4,10 +4,21 @@ Things to use : loops, if, useinput,
 2. The check if the second input is eual to the third input
 3. print a message of what it is : equal or not
 """
-userInput=int(input('Enter the number of times you want to print your name'))
-currentName = ""
-for i in range(userInput):
-    name = input("Enter the name")
-    currentName = name
+userInput=int(input('Trial Number: '))
+
+lastName = ""
+secondLastName = ""
+
+for i in range(1,userInput + 1):
+    name = input("Name: ")
+    lastName = name
+    if i == userInput - 1:
+        secondLastName = name
+
+if secondLastName == lastName:
+    print("Names are the same")
+else:
+    print("Names are different")
+
     
      
