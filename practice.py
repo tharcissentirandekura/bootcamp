273abcf7eef2427ffc30e6ce671574e2d6d7a27c
Bizimana61 <155080993+Bizimana61@users.noreply.github.com>
2023-12-31 02:51:08 +0000

    save

--- practice.py
+++ practice.py
@@ -1,6 +1,13 @@
 """"
 Things to use : loops, if, useinput, 
-1. ask a user to enter howm many times to get asked to enter his name 
+1. ask a user to enter how many times to get asked to enter his name 
 2. The check if the second input is eual to the third input
 3. print a message of what it is : equal or not
 """
+userInput=int(input('Enter the number of times you want to print your name'))
+currentName = ""
+for i in range(userInput):
+    name = input("Enter the name")
+    currentName = name
+    
+     
